c1a0aa10b36a609da4b6a86cae109510c2b21a51
Shenal Fernando <developershenal@gmail.com>
2023-08-28 08:19:10 +0530

    password hasing util class

---
+++ Utils/Hash.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace HealthCare_Plus.Utils
+{
+    internal class Hash
+    {
+        public string HashPassword(string password)
+        {
+            string inputPassword = password;
+            byte[] inputPasswordByte = ASCIIEncoding.ASCII.GetBytes(inputPassword);
+            byte[] hashPassword = new SHA256CryptoServiceProvider().ComputeHash(inputPasswordByte);
+            return ByteArrayToString(hashPassword);
+        }
+
+        private static string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length; i++)
+            {
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+
+        /*
+         * VERIFY HASHED PASSWORD MACHES INPUT PASSWORD
+         * @params inputPassword = user input password
+         * @params passwordHash = database hashed password
+         */
+        public bool VerifyPassword(string inputPassword, string passwordHash)
+        {
+            bool isValid = false;
+
+            byte[] inputPasswordByte = ASCIIEncoding.ASCII.GetBytes(inputPassword);
+            byte[] inputPasswordHash = new SHA256CryptoServiceProvider().ComputeHash(inputPasswordByte);
+
+            if (ByteArrayToString(inputPasswordHash) == passwordHash)
+            {
+                isValid = true;
+            }
+
+            return isValid;
+        }
+    }
+}
