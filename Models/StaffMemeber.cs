c6fae8373841fb7e86f0a9b316f91710d4b3143a
Shenal Fernando <developershenal@gmail.com>
2023-09-07 08:08:33 +0530

    Add query functions to the class

---
+++ Models/StaffMemeber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace HealthCare_Plus.Models
+{
+
+    internal class StaffMemeber: User
+    {
+        //VARIABLES
+        private string insertQuery = "INSERT INTO StaffProfiles " +
+                "(contact_no, address, user_id) " +
+                "VALUES (@contact_no, @address, @user_id);";
+        private string updateQuery = "UPDATE StaffProfiles SET "+
+                    "contact_no = @contact_no, " +
+                    "address = @address " +
+                    "WHERE user_id = @user_id;";
+        private string deleteQuery = "DELETE FROM StaffProfiles WHERE user_id = @user_id";
+
+        public StaffMemeber()
+        {
+            //DEFAULT CONSTRUCTOR
+        }
+
+        public StaffMemeber(
+            string first_name,
+            string last_name,
+            string email,
+            string phone_no,
+            string password,
+            string address,
+            string role): base(first_name, last_name, email, phone_no, password, address, role)
+        {
+           
+        }
+
+        public SqlCommand GetStaffInsertCommand(SqlConnection connection, Int64 user_id)
+        {
+            SqlCommand command = new SqlCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@contact_no", base.PhoneNumber);
+            command.Parameters.AddWithValue("@address", base.Address);
+            command.Parameters.AddWithValue("@user_id", user_id);
+            return command;
+        }
+
+        public SqlCommand GetStaffUpdateCommand(SqlConnection connection, Int64 user_id)
+        {
+            SqlCommand command = new SqlCommand(updateQuery, connection);
+            command.Parameters.AddWithValue("@contact_no", base.PhoneNumber);
+            command.Parameters.AddWithValue("@address", base.Address);
+            command.Parameters.AddWithValue("@user_id", user_id);
+            return command;
+        }
+
+        public SqlCommand GetStaffDeleteCommand(SqlConnection connection, Int64 user_id)
+        {
+            SqlCommand command = new SqlCommand(deleteQuery,connection);
+            command.Parameters.AddWithValue("@user_id", user_id);
+            return command;
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HealthCare_Plus.Utils;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,19 @@ namespace HealthCare_Plus.Models
         private string address;
         private string role;
 
+        //QUERY VARIABLES
+        private string insertQuery = "INSERT INTO Users " +
+                "(first_name, last_name, email, password, role) " +
+                "output INSERTED.ID " +
+                "VALUES (@first_name, @last_name, @email, @password, @role);";
+        private string updateQuery = "UPDATE Users SET " +
+                    "first_name = @first_name, " +
+                    "last_name = @last_name, " +
+                    "email = @email, " +
+                    "role = @role " +
+                    "WHERE id = @user_id;";
+        private string deleteQuery = "DELETE FROM Users WHERE id = @user_id";
+
         public string FirstName { get => first_name; set => first_name = value; }
         public string LastName { get => last_name; set => last_name = value; }
         public string Email { get => email; set => email = value; }
@@ -23,6 +38,11 @@ namespace HealthCare_Plus.Models
         public string Password { get => password; set => password = value; }
         public string Address { get => address; set => address = value; }
         public string Role { get => role; set => role = value; }
+
+        public User()
+        {
+            //DEFATULT CONSTRUCTOR
+        }
 
         public User(string first_name, string last_name, string email, string phone_no, string password, 
             string address, string role) {
@@ -35,6 +55,35 @@ namespace HealthCare_Plus.Models
             this.role = role;
         }
 
+        public SqlCommand GetInsertCommand(SqlConnection connection)
+        {
+            Hash hash = new Hash();
+            string hashedPassword = hash.HashPassword(this.password);
+            SqlCommand command = new SqlCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@first_name", this.first_name);
+            command.Parameters.AddWithValue("@last_name", this.last_name);
+            command.Parameters.AddWithValue("@email", this.email);
+            command.Parameters.AddWithValue("@password", hashedPassword);
+            command.Parameters.AddWithValue("@role", this.role);
+            return command;
+        }
 
+        public SqlCommand GetUpdateCommand(SqlConnection connection, Int64 user_id)
+        {
+            SqlCommand command = new SqlCommand(updateQuery, connection);
+            command.Parameters.AddWithValue("@first_name", this.first_name);
+            command.Parameters.AddWithValue("@last_name", this.last_name);
+            command.Parameters.AddWithValue("@email", this.email);
+            command.Parameters.AddWithValue("@role", this.role);
+            command.Parameters.AddWithValue("@user_id", user_id);
+            return command;
+        }
+
+        public SqlCommand GetDeleteCommand(SqlConnection connection, Int64 user_id)
+        {
+            SqlCommand command = new SqlCommand(deleteQuery, connection);
+            command.Parameters.AddWithValue("@user_id", user_id);
+            return command;
+        }
     }
 }
